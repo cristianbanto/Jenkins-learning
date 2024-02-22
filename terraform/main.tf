3b06778793c0dc5170694771a306dd6dedea305f
cristian banto <cristianbanto@yahoo.com>
2024-02-22 14:27:09 +0200

    terraform pipeline

---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = var.aws_region
+
+}
+
+resource "aws_instance" "test_instance" {
+  ami           = "ami-0c7217cdde317cfec"  
+  instance_type = "t2.micro"
+  tags = {
+    Name = "test-vm"
+  }
+}
+
+resource "aws_iam_role" "test-role" {
+  name               = "test_role"
+  assume_role_policy = data.aws_iam_policy_document.instance_assume_role_policy.json
+}
+
+resource "aws_s3_bucket" "test-bucket" {
+  bucket = "my-tf-test-bucket"
+  region = var.aws_region
+
+  tags = {
+    Name        = "test-bucket"
+    Environment = "Dev"
+  }
+}
