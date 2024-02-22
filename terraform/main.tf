dc2e0e5cae47eec87149e895da1c9e1cc8929c1d
cristian banto <cristianbanto@yahoo.com>
2024-02-22 14:51:28 +0200

    terraform pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "test_instance" {
-  ami           = ami-0c7217cdde317cfec  
+  ami           = "ami-0c7217cdde317cfec"  
   instance_type = "t2.micro"
   tags = {
     Name = "test-vm"
